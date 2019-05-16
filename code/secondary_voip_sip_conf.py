cf798ebbc2878bd9a8ef2702c81546379b792383
Christopher M Johnston <40300884+cj667113@users.noreply.github.com>
2019-05-16 00:42:36 -0500

    Create secondary_voip_sip_conf.py

---
+++ code/secondary_voip_sip_conf.py
@@ -0,0 +1,68 @@
+import mysql.connector
+import os
+print("trying")
+cnx = mysql.connector.connect(user='primary_test', password='test_password', host='10.0.0.10', port='3306', database='Authentication')
+print("done")
+mycursor = cnx.cursor()
+sql = "SELECT * FROM users WHERE Extension"
+mycursor.execute(sql)
+myresult = mycursor.fetchall()
+cnx.close()
+collect=[]
+for x in myresult:
+        precollect=[]
+        for y in x:
+                y=(str(unicode(y)).strip("u'"))
+                precollect.append(y)
+        collect.append(precollect)
+refined=[]
+for x in collect:
+        if 2000<=int(x[3])<=2999:
+                refined.append(x)
+
+
+base_setup="""
+[general]
+alwaysauthreject=yes
+videosupport=yes
+disallow=all
+allow=ulaw,alaw,h263,h264,h263p
+register => secondary:welcome@10.0.0.7/primary
+register => secondary:welcome@10.30.0.4/tertiary
+
+[primary]
+type=friend
+secret=welcome
+context=incoming
+host=dynamic
+context=incoming
+qualify=yes
+
+[tertiary]
+type=friend
+secret=welcome
+context=incoming
+host=dynamic
+context=incoming
+qualify=yes
+"""
+sip_list=[]
+for x in refined:
+        exten="""
+["""+x[3]+"""]
+type=friend
+callerid="""+'"'+x[1]+'"'+"""
+secret="""+x[2]+"""
+host=dynamic
+context=LocalExt
+"""
+        sip_list.append(exten)
+for x in sip_list:
+        print(x)
+f = open("/etc/asterisk/sip.conf","w+")
+f.write(base_setup)
+for x in sip_list:
+        f.write(x)
+f.close()
+asterisk_restart="sudo systemctl restart asterisk"
+os.system(asterisk_restart)
